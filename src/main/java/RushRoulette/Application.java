e8c218aeaaea23f980304c9c3a103fe1784233d5
hugocruz <hugomcruz.04@gmail.com>
2023-12-07 21:09:45 +0000

    Everything working with the right font

--- src/main/java/RushRoulette/Application.java
+++ src/main/java/RushRoulette/Application.java
@@ -13,7 +13,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// Press Shift twice to open the Search Everywhere dialog and type show whitespaces,
 // then press Enter. You can now see whitespace characters in your code.
 public class Application {
     private final LanternaGUI gui;
