9bcfe820cfca8eb3cba881ea4913de610c0a32ea
YaBoiKazz <tomastelmo10@gmail.com>
2023-12-08 04:21:20 +0000

    did some lvl design in all the lvls

--- src/main/java/RushRoulette/Viewer/Menu/MenuViewer.java
+++ src/main/java/RushRoulette/Viewer/Menu/MenuViewer.java
@@ -9,11 +9,12 @@ public class MenuViewer extends Viewer<Menu> {
     public MenuViewer(Menu menu){super(menu);}
     @Override
     public void drawElements(GUI gui){
-        gui.drawText(new Position(5,5), "Menu", "#FFFFFF" );
+        gui.drawText(new Position(12,5), "RUSH ROULETTE!", "#FFFFFF" );
+        gui.drawText(new Position(12,7), "-----Menu-----", "#FFFFFF" );
 
         for(int i=0; i<getModel().getNumberEntries();i++){
             gui.drawText(
-                    new Position(5, 7+i),
+                    new Position(17, 9+i),
                     getModel().getEntry(i),
                     getModel().isSelected(i)? "#FFD700" : "#FFFFFF");
         }
